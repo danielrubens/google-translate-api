b8f68718adce1f0e3f6b244bb3c00d9c99dcd34c
danielrubens <danielrubensdaniel@gmail.com>
2023-04-01 00:40:03 -0300

    feat: script to create rafce

---
+++ createComponents.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const path = require('path')
+
+const components = process.argv.slice(2)
+if(!components.length){
+    console.log('Please specify at lest one component to create')
+    process.exit(1)
+}
+
+const componentDir = path.join(__dirname, 'src', 'components')
+
+components.forEach((component) => {
+    const rafce = `import React from 'react';
+    
+    const ${component} = () => {
+        return(
+            <div>
+                {/* Content */}}
+            </div>
+        );
+    };
+    
+    export default ${component};`
+    
+    const filePath = path.join(componentDir, `${component}.js`)
+    fs.writeFileSync(filePath, rafce);
+    console.log(`Component ${component}.js created successfully!`)
+})
