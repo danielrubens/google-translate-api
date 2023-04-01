553547d31bcb815f88f016d42a145fa7af6d0712
danielrubens <danielrubensdaniel@gmail.com>
2023-04-01 16:47:43 -0300

    test: ensure TextBox conditional render on both input and output

--- src/__tests__/TextBox.test.js
+++ src/__tests__/TextBox.test.js
@@ -6,19 +6,23 @@ import TextBox from '../components/TextBox';
 describe('TextBox Component', () => {
   it('should render "Enter Text" on input placeholder', () => {
     render(<TextBox style='input' />);
-    const input = screen.getByPlaceholderText('Enter Text');
+    const input = screen.queryByPlaceholderText('Enter Text');
+    const output = screen.queryByPlaceholderText('Translation');
     expect(input).toBeInTheDocument();
+    expect(output).not.toBeInTheDocument()
   });
 
   it('should render "Translation" on output placeholder', () => {
     render(<TextBox style='output' />);
-    const output = screen.getByPlaceholderText('Translation');
+    const output = screen.queryByPlaceholderText('Translation');
+    const input = screen.queryByPlaceholderText('Enter Text');
     expect(output).toBeInTheDocument();
+    expect(input).not.toBeInTheDocument()
   });
 
   it('should allow user to type on textarea input', () => {
     render(<TextBox style='input' />);
-    const input = screen.getByPlaceholderText('Enter Text');
+    const input = screen.queryByPlaceholderText('Enter Text');
     userEvent.type(input, 'Hello, World!');
     expect(input).toHaveValue('Hello, World!');
   });
