616f845c7706e28f4985a621f07fbca2bd0dc556
danielrubens <danielrubensdaniel@gmail.com>
2023-04-02 00:18:04 -0300

    test: arrows component with svg called

---
+++ src/__tests__/Arrows.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import Arrows from '../components/Arrows';
+
+describe('Arrows component', () => {
+  it('renders the SVG arrows', () => {
+    render(<Arrows />);
+    const svgElement = screen.getByTestId('arrows');
+    const pathElement = screen.getByTestId('arrows-path');
+    const pathAttrs = ['d', 'M6.99 11L3 15l3.99 4v-3H14v-2H6.99v-3zM21 9l-3.99-4v3H10v2h7.01v3L21 9z']
+    expect(svgElement).toBeInTheDocument(); 
+    expect(pathElement).toHaveAttribute(...pathAttrs);
+  });
+});
