a7bc694ac4ea42daa370a930cf73f429c6765796
danielrubens <danielrubensdaniel@gmail.com>
2023-04-02 02:13:01 -0300

    test: configure mock store for arrow component

--- src/__tests__/Arrows.test.js
+++ src/__tests__/Arrows.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen, render, configure } from '@testing-library/react';
 import Arrows from '../components/Arrows';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store'
 import App from '../App';
+
+const mockStore = configureMockStore()
 
 describe('Arrows component', () => {
   it('renders the SVG arrows', () => {
-    render(<Arrows />);
+    const store = mockStore({user: {input: 'English', output: 'Portuguese'}})
+    render(
+      <Provider store={store}>
+        <Arrows />
+      </Provider>
+    );
     const svgElement = screen.getByTestId('arrows');
     const pathElement = screen.getByTestId('arrows-path');
     const pathAttrs = ['d', 'M6.99 11L3 15l3.99 4v-3H14v-2H6.99v-3zM21 9l-3.99-4v3H10v2h7.01v3L21 9z']
@@ -14,7 +23,12 @@ describe('Arrows component', () => {
   });
 
   it('ensures Arrows component is called inside the App', () => {
-    render(<App />);
+    const store = mockStore({user: {input: 'English', output: 'Portuguese'}})
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     const arrowsComponent = screen.getByTestId('arrows');
     expect(arrowsComponent).toBeInTheDocument();
   });
