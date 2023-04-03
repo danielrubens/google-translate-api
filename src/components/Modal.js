4ec24661fac3a95eef433c43d3e59432ff5341c5
danielrubens <danielrubensdaniel@gmail.com>
2023-04-03 01:21:47 -0300

    remove: unecessary getLanguages function inside modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,29 +9,6 @@ const Modal = () => {
     const dispatch = useDispatch()
     const handleDispatch = () => { dispatch({type: 'MODAL', payload: null}) }
 
-    const getLanguages = async () => {
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '9f8dc9bd0cmshd7a8d3b1e5d4bd6p1bef81jsnf5ddcaa720dc',
-                'X-RapidAPI-Host': 'google-translator9.p.rapidapi.com'
-            }
-        };
-        try{
-            const response =  await fetch('https://google-translator9.p.rapidapi.com/v2/languages', options)
-            const json = await response.json()
-            return json.data.languages
-        }
-        catch(error){
-            return {error}
-        }
-    }
-
-    // const changeLanguage = async() =>{
-    //     const response = await getLanguages()
-    //     setLanguages(response)
-    // }
-
     useEffect(() => {getLanguages().then((response) => setLanguages(response))}, [])
     languages.forEach((i) => console.log(i.language))
 
